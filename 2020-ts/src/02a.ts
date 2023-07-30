c817605e0fae990fe9d2467639028436426e4122
Kunal Kundu <kunal99kundu@gmail.com>
2023-07-30 13:47:01 +0530

    2020-ts: refactor

--- 2020-ts/src/02a.ts
+++ 2020-ts/src/02a.ts
@@ -3,24 +3,13 @@ import fs from 'fs/promises'
 async function main() {
 	const data = await fs.readFile('inputs/02.txt', { encoding: 'utf8' })
 
-	const arr = data
-		.split('\n')
-		.map(line => {
-			let [limits, char, password] = line.split(' ', 3)
-			char = char[0]
-			let [lo, hi] = limits.split('-', 2).map(lim => parseInt(lim))
-
-			return {
-				lo,
-				hi,
-				char,
-				password,
-			}
-		})
-		.filter(({ lo, hi, char, password }) => {
-			const occurences = password.split('').filter(ch => ch === char).length
-			return occurences >= lo && occurences <= hi
-		})
+	const arr = data.split('\n').filter(line => {
+		let [limits, char, password] = line.split(' ', 3)
+		char = char[0]
+		const [lo, hi] = limits.split('-', 2).map(lim => parseInt(lim))
+		const occurences = password.split('').filter(c => c === char).length
+		return occurences >= lo && occurences <= hi
+	})
 
 	console.log('valid passwords', arr.length)
 }
--- 2020-ts/src/02b.ts
+++ 2020-ts/src/02b.ts
@@ -3,25 +3,15 @@ import fs from 'fs/promises'
 async function main() {
 	const data = await fs.readFile('inputs/02.txt', { encoding: 'utf8' })
 
-	const arr = data
-		.split('\n')
-		.map(line => {
-			let [limits, char, password] = line.split(' ', 3)
-			char = char[0]
-			let [pos1, pos2] = limits.split('-', 2).map(lim => parseInt(lim))
+	const arr = data.split('\n').filter(line => {
+		let [limits, char, password] = line.split(' ', 3)
+		const [pos1, pos2] = limits.split('-', 2).map(lim => parseInt(lim))
+		char = char[0]
 
-			return {
-				pos1,
-				pos2,
-				char,
-				password,
-			}
-		})
-		.filter(({ pos1, pos2, char, password }) => {
-			const cond1 = password[pos1 - 1] === char ? 1 : 0
-			const cond2 = password[pos2 - 1] === char ? 1 : 0
-			return Boolean(cond1 ^ cond2)
-		})
+		const cond1 = password[pos1 - 1] === char
+		const cond2 = password[pos2 - 1] === char
+		return cond1 != cond2
+	})
 
 	console.log('valid passwords', arr.length)
 }
